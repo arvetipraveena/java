d78c464122db914615ad36465ac3784050856f9c
arvetipraveena <arvetipraveena66@gmail.com>
2025-06-02 16:00:54 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class evenodd{
+public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+int n=sc.nextInt();
+if(n%2==0){
+System.out.print("even");}
+else{
+System.out.print("odd");}}}
