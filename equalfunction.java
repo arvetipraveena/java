b156faf2e035d50104f9d0c4121d511a6c90acbe
arvetipraveena <arvetipraveena66@gmail.com>
2025-06-02 15:54:41 +0530

    Add files via upload

---
+++ equalfunction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class equalfunction{
+public static void main(String[] args){
+ Scanner sc=new Scanner(System.in);
+ String a=sc.nextLine();
+ String b=sc.nextLine();
+   if(a.equals(b)){
+   System.out.print("both strings are equal");
+   }
+   else{
+   System.out.println("strings are not equal");
+  }
+ }
+ }
